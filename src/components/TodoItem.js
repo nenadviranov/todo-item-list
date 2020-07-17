d4934cd513ecbe31600a88a9cb0cab3056ce0ff1
nenadviranov <nenad.viranov@gmail.com>
2020-07-17 15:36:26 +0200

    edit and delete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -8,10 +8,10 @@ export default class TodoItem extends Component {
                 <h6>{title}</h6>
                 <div className="todo-icon">
                     <span className="mx-2 text-success" onClick={handleEdit}>
-                        <i>Edit +</i>
+                        <i>Edit</i>
                     </span>
                     <span className="mx-2 text-danger" onClick={handleDelete}>
-                    <i>Delete X</i>
+                    <i>Delete</i>
                     </span>
                 </div>
             </li>
